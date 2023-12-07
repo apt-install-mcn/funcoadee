4010927060368a13c6912143a3f550ac079fb45a
moisescn <moosescn20@gmail.com>
2023-12-07 09:12:54 -0500

    home

--- interaction/menu.js
+++ interaction/menu.js
@@ -31,6 +31,9 @@ window.addEventListener('scroll', function() {
     const navbar = document.querySelector('.navbar');
     const scrolled = window.scrollY > 0;
 
+    console.log('Scroll Y:', window.scrollY); // Verifica la posición de scroll
+    console.log('Scrolled:', scrolled); // Verifica si scrolled es true o false
+
     if (scrolled) {
         navbar.classList.add('scrolled');
     } else {
@@ -39,3 +42,16 @@ window.addEventListener('scroll', function() {
 });
 
 
+
+
+ $(document).ready(function(){
+     // Inicia el carrusel
+     $('#carouselExampleIndicators').carousel();
+
+     // Establece el intervalo para el cambio automático
+     $('#carouselExampleIndicators').carousel({
+       interval: 3000 // Cambia cada 3 segundos (3000 milisegundos)
+     });
+   });
+
+  
